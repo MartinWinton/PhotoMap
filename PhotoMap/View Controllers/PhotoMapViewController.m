748981dc14edaee2c16c2675a710fe10a178c332
Martin Winton <mwinton@fb.com>
2018-07-11 10:59:46 -0700

    photomap lab

---
+++ PhotoMap/View Controllers/PhotoMapViewController.m
@@ -0,0 +1,114 @@
+//
+//  PhotoMapViewController.m
+//  PhotoMap
+//
+//  Created by emersonmalca on 7/8/18.
+//  Copyright © 2018 Codepath. All rights reserved.
+//
+
+#import "PhotoMapViewController.h"
+#import <MapKit/MapKit.h>
+#import "LocationsViewController.h"
+
+@interface PhotoMapViewController () <UIImagePickerControllerDelegate,UINavigationControllerDelegate,LocationsViewControllerDelegate, MKMapViewDelegate>
+@property (weak, nonatomic) IBOutlet MKMapView *jessicaMapView;
+@property (weak, nonatomic) UIImage *chosenImage;
+
+@end
+
+@implementation PhotoMapViewController
+- (IBAction)didClickCamera:(id)sender {
+    
+    UIImagePickerController *imagePickerVC = [UIImagePickerController new];
+    imagePickerVC.delegate = self;
+    imagePickerVC.allowsEditing = YES;
+    imagePickerVC.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+    
+    [self presentViewController:imagePickerVC animated:YES completion:nil];
+    
+    
+}
+
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary<NSString *,id> *)info {
+    
+    // Get the image captured by the UIImagePickerController
+    UIImage *originalImage = info[UIImagePickerControllerOriginalImage];
+    UIImage *editedImage = info[UIImagePickerControllerEditedImage];
+    
+    self.chosenImage = editedImage;
+    
+    // Do something with the images (based on your use case)
+    
+    // Dismiss UIImagePickerController to go back to your original view controller
+    [self dismissViewControllerAnimated:YES completion:^{
+        
+        [self performSegueWithIdentifier:@"tagSegue" sender:nil];
+        
+        
+    }];
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    MKCoordinateRegion sfRegion = MKCoordinateRegionMake(CLLocationCoordinate2DMake(37.783333, -122.416667), MKCoordinateSpanMake(0.1, 0.1));
+    [self.jessicaMapView setRegion:sfRegion animated:false];
+    
+    self.jessicaMapView.delegate = self;
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    
+    // Dispose of any resources that can be recreated.
+}
+
+
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    
+    LocationsViewController *locationController = [segue destinationViewController];
+    
+ 
+    
+        locationController.delegate = self;
+
+    }
+    
+
+    
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+
+
+- (void)didPickLocationWithLatitude:(NSNumber *)latitude longitude:(NSNumber *)longitude{
+    CLLocationCoordinate2D coordinate = CLLocationCoordinate2DMake(latitude.floatValue, longitude.floatValue);
+    
+    MKPointAnnotation *annotation = [MKPointAnnotation new];
+    annotation.coordinate = coordinate;
+    annotation.title = @"Picture!";
+    [self.jessicaMapView addAnnotation:annotation];
+    
+    [self.navigationController popToViewController:self animated:NO];
+    
+    
+}
+
+- (MKAnnotationView *)mapView:(MKMapView *)mapView viewForAnnotation:(id<MKAnnotation>)annotation {
+    MKPinAnnotationView *annotationView = (MKPinAnnotationView*)[mapView dequeueReusableAnnotationViewWithIdentifier:@"Pin"];
+    if (annotationView == nil) {
+        annotationView = [[MKPinAnnotationView alloc] initWithAnnotation:annotation reuseIdentifier:@"Pin"];
+        annotationView.canShowCallout = true;
+        annotationView.leftCalloutAccessoryView = [[UIImageView alloc] initWithFrame:CGRectMake(0.0, 0.0, 50.0, 50.0)];
+    }
+    
+    UIImageView *imageView = (UIImageView*)annotationView.leftCalloutAccessoryView;
+    imageView.image = [UIImage imageNamed:@"camera"];
+    
+    return annotationView;
+}
+
+
+@end
